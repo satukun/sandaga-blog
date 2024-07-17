fd5b26bde91bc713e0e73fe71cdd83b0ff721c74
YousukeSatou <y.freestyle@gmail.com>
2024-07-17 17:19:38 +0900

    update

--- app/component/Home/HeroSection.js
+++ app/component/Home/HeroSection.js
@@ -1,13 +1,12 @@
 const HeroSection = () => {
-    return (
-      <section className="hero bg-blue-500 text-white py-20">
-        <div className="container mx-auto text-center">
-          <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio</h1>
-          <p className="text-xl mb-8">Im a Frontend Engineer passionate about building beautiful and functional web applications.</p>
+  return (
+    <section className="hero bg-blue-500 text-white py-20">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio</h1>
+        <p className="text-xl mb-8">I&apos;m a Frontend Engineer passionate about building beautiful and functional web applications.</p>
+      </div>
+    </section>
+  );
+};
 
-        </div>
-      </section>
-    );
-  };
-  
-  export default HeroSection;
+export default HeroSection;
